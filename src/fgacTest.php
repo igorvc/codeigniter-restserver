abac2279b7c5be2872ea91555ea246b7d903bd57
Igor Vitorio Custodio <igorvc@icmc.usp.br>
2021-10-20 13:55:23 -0300

    working on #task1

--- src/fgacTest.php
+++ src/fgacTest.php
@@ -14,20 +14,22 @@
 
 require APPPATH . '../vendor/autoload.php';
 
-
+$options = array(
+   'timeout' => 60000,
+ );
    
 $apikey = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa';
     
-    //executando a consulta
-    $response = Requests::get('https://ws.icmc.usp.br/wsicmc/Grad/listarCursos', array('ICMC-SAPI-KEY' => $apikey), $options);
+//executando a consulta
+$response = Requests::get('http://localhost/cirs/Api/users', array('X-API-KEY' => $apikey), $options);
 
-    //processando a resposta
-    if(isset($response->success) && $response->success == true){
-       echo 'Ok!';
-       echo 'Esta é a resposta: ', print_r($response->body, true); 
-    } else {
-       echo 'Erro';
-       echo 'Esta foi o erro apontado: ', print_r($response->body, true);
-    }
+//processando a resposta
+if(isset($response->success) && $response->success == true){
+   echo 'Ok!';
+   echo 'Response: ', print_r($response->body, true); 
+} else {
+   echo 'Error';
+   echo 'Response: ', print_r($response->body, true);
+}
 
     
