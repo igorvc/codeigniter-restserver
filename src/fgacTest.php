954504100485175568b16996351cd5b2712cb9c0
Igor Vitorio Custodio <igorvc@icmc.usp.br>
2021-10-20 15:02:33 -0300

    working on #task1

--- src/fgacTest.php
+++ src/fgacTest.php
@@ -33,7 +33,61 @@ $apikey = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa';
 //Consult
 $response = Requests::get('http://localhost/cirs/index.php/Api/users', array('X-API-KEY' => $apikey), $options);
 
-logTest('In this example, we need to receive Error Response: {"status":false,"error":"Invalid API key "}');
+logTest('(1) In this example, we need to receive Error Response: {"status":false,"error":"Invalid API key "}');
+//processing
+if(isset($response->success) && $response->success == true){
+   logTest('Ok!');
+   logTest('Response: ' . print_r($response->body, true)); 
+} else {
+   logTest('Error');
+   logTest('Response: ' . print_r($response->body, true));
+}
+
+###################################################################################################################
+# Test 2 -  Using root (id=1) Key                                                                                 #
+################################################################################################################### 
+$apikey = 'a50348e70dc84e99496db527055a65db';
+    
+//Consult
+$response = Requests::get('http://localhost/cirs/index.php/Api/users', array('X-API-KEY' => $apikey), $options);
+
+logTest('(2) Using root (id=1) Key, expected Ok!');
+//processing
+if(isset($response->success) && $response->success == true){
+   logTest('Ok!');
+   logTest('Response: ' . print_r($response->body, true)); 
+} else {
+   logTest('Error');
+   logTest('Response: ' . print_r($response->body, true));
+}
+
+###################################################################################################################
+# Test 3 -  2º Key access specific Controller                                                                     #
+################################################################################################################### 
+$apikey = 'c5ba6c0463e35622f9c89bbda027b9b9';
+    
+//Consult
+$response = Requests::get('http://localhost/cirs/index.php/Api/users', array('X-API-KEY' => $apikey), $options);
+
+logTest('(3) Using Key (id=2), expected Ok!');
+//processing
+if(isset($response->success) && $response->success == true){
+   logTest('Ok!');
+   logTest('Response: ' . print_r($response->body, true)); 
+} else {
+   logTest('Error');
+   logTest('Response: ' . print_r($response->body, true));
+}
+
+###################################################################################################################
+# Test 4 -  3º Key access specific Controller/Method                                                                     #
+################################################################################################################### 
+$apikey = 'a4b5d7002911890ae82acc3e54392c5f';
+    
+//Consult
+$response = Requests::get('http://localhost/cirs/index.php/Api/users', array('X-API-KEY' => $apikey), $options);
+
+logTest('(4) Using Key (id=3), expected Ok!');
 //processing
 if(isset($response->success) && $response->success == true){
    logTest('Ok!');
@@ -44,7 +98,59 @@ if(isset($response->success) && $response->success == true){
 }
 
 
+###################################################################################################################
+# Test 5 -  Using root (id=1) Key                                                                                 #
+################################################################################################################### 
+$apikey = 'a50348e70dc84e99496db527055a65db';
+    
+//Consult
+$response = Requests::get('http://localhost/cirs/index.php/Api/usersRestrict', array('X-API-KEY' => $apikey), $options);
 
+logTest('(2) Using root (id=1) Key on usersRestrict, expected Ok!');
+//processing
+if(isset($response->success) && $response->success == true){
+   logTest('Ok!');
+   logTest('Response: ' . print_r($response->body, true)); 
+} else {
+   logTest('Error');
+   logTest('Response: ' . print_r($response->body, true));
+}
+
+###################################################################################################################
+# Test 6 -  2º Key access specific Controller                                                                     #
+################################################################################################################### 
+$apikey = 'c5ba6c0463e35622f9c89bbda027b9b9';
+    
+//Consult
+$response = Requests::get('http://localhost/cirs/index.php/Api/usersRestrict', array('X-API-KEY' => $apikey), $options);
+
+logTest('(6) Using Key (id=2) on usersRestrict, expected Ok!');
+//processing
+if(isset($response->success) && $response->success == true){
+   logTest('Ok!');
+   logTest('Response: ' . print_r($response->body, true)); 
+} else {
+   logTest('Error');
+   logTest('Response: ' . print_r($response->body, true));
+}
+
+###################################################################################################################
+# Test 7 -  3º Key access specific Controller/Method                                                                     #
+################################################################################################################### 
+$apikey = 'a4b5d7002911890ae82acc3e54392c5f';
+    
+//Consult
+$response = Requests::get('http://localhost/cirs/index.php/Api/usersRestrict', array('X-API-KEY' => $apikey), $options);
+
+logTest('(7) Using Key (id=3) on usersRestrict, expected {"status":false,"error":"This API key does not have access to the requested controller"}');
+//processing
+if(isset($response->success) && $response->success == true){
+   logTest('Ok!');
+   logTest('Response: ' . print_r($response->body, true)); 
+} else {
+   logTest('Error');
+   logTest('Response: ' . print_r($response->body, true));
+}
 
 
 
